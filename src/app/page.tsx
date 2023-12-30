ca882b12b21586429414a648b4dff371a6526ee3
yeoni sugar <rkdgkdus522@naver.com>
2023-12-30 17:41:49 +0900

    [ADD] : component & router 초기 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import React from "react";
+import Header from "@/components/Header";
+import Intro from "@/components/Intro";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+
 export default function Home() {
   return (
-    <main>
-      <div>
-        home
-      </div>
-    </main>
-  )
+    <>
+      <Header />
+      <main id="main" role="main">
+        <Intro />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  );
 }
