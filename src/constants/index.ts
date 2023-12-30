960cda3bd007f9f9769787bca8bf07a48ed0f87f
yeoni sugar <rkdgkdus522@naver.com>
2023-12-31 01:28:00 +0900

    [ADD] : header 구현

---
+++ src/constants/index.ts
@@ -0,0 +1,28 @@
+import { IHeaderMenu } from "@/types/header";
+
+export const headerMenu: Array<IHeaderMenu> = [
+  {
+    title: "intro",
+    url: "#intro",
+  },
+  {
+    title: "about",
+    url: "#about",
+  },
+  {
+    title: "skill",
+    url: "#skill",
+  },
+  {
+    title: "timeline",
+    url: "#timeline",
+  },
+  {
+    title: "project",
+    url: "#project",
+  },
+  {
+    title: "contact",
+    url: "#contact",
+  },
+];
